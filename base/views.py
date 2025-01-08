35c7ede02d74d6cad53dd501f3517ab404c6a8a1
munhyunsu <munhyunsu@gmail.com>
2025-01-08 23:10:02 +0900

    add URL_PREFIX to hx-get of base/views.py

--- base/views.py
+++ base/views.py
@@ -5892,7 +5892,7 @@ def delete_shift_comment_file(request):
         messages.success(request, _("File deleted successfully"))
     else:
         messages.warning(request, _("You don't have permission"))
-        script = f"""<span hx-get="/view-shift-comment/{shift_id}/" hx-trigger="load" hx-target="#commentContainer" data-target="#activitySidebar"></span>"""
+        script = f"""<span hx-get="/{settings.URL_PREFIX}view-shift-comment/{shift_id}/" hx-trigger="load" hx-target="#commentContainer" data-target="#activitySidebar"></span>"""
     return HttpResponse(script)
 
 
